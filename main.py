09a35cad34793a6ed67bd7df5cb63bf6addadee3
Karan Ahirwar <114184956+karanahirwar1996@users.noreply.github.com>
2023-05-24 08:29:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import posemails
 import pandas as pd
 df1=indiatimes.india_times()
 df2=business.positive_news()
+df1["Source"]="EconomicTimes"
+df2["Source"]="Business-standard"
 concat_df=pd.concat([df1,df2],ignore_index=True)
 sorted_df=concat_df.loc[(concat_df['Deep Score']>45)&(concat_df['Normal Score']>0)]
 posemails.pos_email(sorted_df,concat_df)
