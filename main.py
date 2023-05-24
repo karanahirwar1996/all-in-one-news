2394034a14bed65214492eff665c9eaaa794be81
Karan Ahirwar <114184956+karanahirwar1996@users.noreply.github.com>
2023-05-24 21:55:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import indiatimes
 import business
 import posemails
 import pandas as pd
+import negemails
 df1=indiatimes.india_times()
 df2=business.positive_news()
 df1["Source"]="EconomicTimes"
 df2["Source"]="Business-standard"
 concat_df=pd.concat([df1,df2],ignore_index=True)
 sorted_df=concat_df.loc[(concat_df['Deep Score']>45)&(concat_df['Normal Score']>0)]
+neg_df=concat_df.loc[(concat_df['Deep Score']<=0)&(concat_df['Normal Score']<=0)]
 posemails.pos_email(sorted_df,concat_df)
+negemails.neg_email(neg_df,concat_df)
+
