099b4d8e733546cb5387ff14e7b30bf71ded7df9
Karan Ahirwar <114184956+karanahirwar1996@users.noreply.github.com>
2023-05-25 22:16:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ df2=business.positive_news()
 df1["Source"]="EconomicTimes"
 df2["Source"]="Business-standard"
 concat_df=pd.concat([df1,df2],ignore_index=True)
-concat_df["URL"]=concat_df["URL"].apply(shorturl.short_link())
+concat_df["URL"]=concat_df["URL"].apply(shorturl.short_link)
 sorted_df=concat_df.loc[(concat_df['Deep Score']>45)&(concat_df['Normal Score']>0)]
 neg_df=concat_df.loc[(concat_df['Deep Score']<=0)&(concat_df['Normal Score']<=0)]
 posemails.pos_email(sorted_df,concat_df)
