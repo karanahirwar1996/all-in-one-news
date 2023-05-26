ee87383c0eed9fa0aa269361985451f0d6ecb873
Karan Ahirwar <114184956+karanahirwar1996@users.noreply.github.com>
2023-05-26 22:11:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ import posemails
 import pandas as pd
 import negemails
 import shorturl
+import moneycontrol
 df1=indiatimes.india_times()
 df2=business.positive_news()
+df3=moneycontrol.moneycontrol()
 df1["Source"]="EconomicTimes"
 df2["Source"]="Business-standard"
-concat_df=pd.concat([df1,df2],ignore_index=True)
+df3["Source"]="MoneyControl"
+concat_df=pd.concat([df1,df2,df3],ignore_index=True)
 concat_df["URL"]=concat_df["URL"].apply(shorturl.short_link)
 sorted_df=concat_df.loc[(concat_df['Deep Score']>50)&(concat_df['Normal Score']>=0)]
+sorted_df = sorted_df.sort_values(by='Deep Score', ascending=False)
 neg_df=concat_df.loc[(concat_df['Deep Score']<=0)&(concat_df['Normal Score']<=0)]
+neg_df = neg_df.sort_values(by='Deep Score', ascending=False)
 posemails.pos_email(sorted_df,concat_df)
 negemails.neg_email(neg_df,concat_df)
